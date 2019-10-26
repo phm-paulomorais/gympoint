d8ce4ea368d0e6aabe29a6fbc522b62fcce9aeef
Paulo Morais <phm.paulomorais@gmail.com>
2019-10-26 18:37:51 -0300

    Projeto GYMPOINT

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,80 @@
+import * as Yup from 'yup';
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const studentsExists = await Student.findOne({
+      where: { email: req.body.email }
+    });
+
+    if (studentsExists) {
+      return res.status(400).json({ error: 'Student already exists' });
+    }
+
+    const { id, name, email, age, weight, height } = await Student.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id, email } = req.body;
+
+    const student = await Student.findByPk(id);
+
+    if (email && email !== student.email) {
+      const studentExists = await Student.findOne({ where: { email } });
+      if (studentExists) {
+        return res.status(400).json({ error: 'Student already exists' });
+      }
+    }
+
+    const { name, age, weight, height } = await student.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height
+    });
+  }
+}
+
+export default new StudentController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '48d52a4b09442236c470be1e91500351',
+  expiresIn: '7d'
+};
